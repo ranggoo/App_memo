5fad1f3790e21f8cc9038907be0e02d73d344362
amy <melow2@naver.com>
2022-01-11 20:00:33 +0900

    #4 AddFragment  코드 리팩토링 및 mainfragment로 값 전달.

--- app/src/main/java/com/ranggoo/app1_memo/MemoAddFragment.kt
+++ app/src/main/java/com/ranggoo/app1_memo/MemoAddFragment.kt
@@ -7,6 +7,8 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import com.ranggoo.app1_memo.databinding.FragmentMemoAdd1Binding
 import java.text.SimpleDateFormat
@@ -18,67 +20,40 @@ class MemoAddFragment : Fragment() {
     private var _binding: FragmentMemoAdd1Binding? = null
     private val binding get() = _binding!!
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         _binding = FragmentMemoAdd1Binding.inflate(inflater, container, false)
-
-        binding.btnAddBack.setOnClickListener {
-            findNavController().navigate(R.id.action_memoAddFragment_to_mainFragment)
-        }
-        binding.btnSave.setOnClickListener {
-            findNavController().navigate(R.id.action_memoAddFragment_to_mainFragment)
-            Toast.makeText(context, "저장되었습니다", Toast.LENGTH_SHORT).show()
-        }
-
         return binding.root
-
-
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
-        when (item.itemId) {
+    private fun initView() = with(binding){
+        btnSave.setOnClickListener {
+            //사용자가 입력한 내용 가지고 온다.
+            val memoSubject = binding.addMemoSubject.text.toString().trim()
+            val memoText = binding.addMemoText.text.toString().trim()
 
-            //저장버튼
-            R.id.btn_save -> {
-                //사용자가 입력한 내용 가지고 온다.
-                val memoSubject = binding.addMemoSubject.text
-                val memoText = binding.addMemoText.text
+            // 글내용 디비 저장.
+            DBHelper(requireContext())
+                .insertMemo(
+                    memoSubject = memoSubject,
+                    memoText = memoText
+                )
 
-                //쿼리문
-                val sql = """
-                    insert into MemoTable (memo_subject, memo_text, memo_date)
-                    values(?, ?, ?)
-                """.trimIndent()
+            // 프래그먼트로 값전달.
+            val bundle = bundleOf("memo_subject" to memoSubject, "memo_content" to memoText)
+            setFragmentResult(requestKey = "ADD", result = bundle)
 
-                //데이터베이스 오픈
-                val helper = DBHelper(requireContext())
-
-                //현재시간을 구하기
-                val sdf = SimpleDateFormat("yyyy-MM-dd")
-                val now = sdf.format(Date())
-
-                //?에 세팅될 값
-                var arg1 = arrayOf(memoSubject,memoText,now)
-
-                //저장
-                helper.writableDatabase.execSQL(sql,arg1)
-
-
-                //데이타베이스 닫기
-                helper.writableDatabase.close()
-                activity?.finish()
-            }
+            // 이전 프래그먼트로 이동.
+            findNavController().navigateUp()
         }
-
-        return super.onOptionsItemSelected(item)
     }
-
 
 
 }
