75d825d3a8857955add4ad3155bd0b4d866d5204
amy <melow2@naver.com>
2022-01-11 20:00:03 +0900

    #3 MainFragment 코드 리팩토링 및 리스너 등록.

--- app/src/main/java/com/ranggoo/app1_memo/MainFragment.kt
+++ app/src/main/java/com/ranggoo/app1_memo/MainFragment.kt
@@ -1,19 +1,14 @@
 package com.ranggoo.app1_memo
 
-import android.app.Activity
-import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.MainThread
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.ranggoo.app1_memo.databinding.FragmentMainBinding
-import com.ranggoo.app1_memo.databinding.MainRecyclerRowBinding
 
 class MainFragment : Fragment() {
 
@@ -25,45 +20,48 @@ class MainFragment : Fragment() {
     val dateList = ArrayList<String>()
     val idxList = ArrayList<Int>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setFragmentResultListener는 onCreate에서 등록.
+        // 왜냐하면 onCreate는 1번만 실행이 됨.
+        // onCreate -> onCreateView -> onViewCreated -> onDestroyView(화면이사라짐) -> onDestroy(메모리에서 완전히삭제) 순서로 실행되는데,
+        // onCreateView ~ onDestroyView 까지 프래그먼트가 이동할때마다 발생.(화면을 다시 그렸다가 지우는 코드임)
+        // 만약에 onCreateView에 setFragmentResultListener가 있으면, 화면이 그려질때마다 리스너를 등록하기 때문에, 넘겨주는 bundle 값을 받을 수 없음
+        // 따라서 항상 리슨을 하고 있어야 되기 때문에, onCreate에서 등록.
+        setFragmentResultListener(requestKey = "ADD") { key, bundle ->
+            val memoSubject = bundle.getString("memo_subject")
+            val memoContent = bundle.getString("memo_content")
+            Toast.makeText(requireContext(), "제목: $memoSubject, 내용: $memoContent", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        binding.btnAdd.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+        btnAdd.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_memoAddFragment)
         }
 
-        binding.rv.setOnClickListener {
+        rv.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_memoReadFragment)
         }
-
-        return binding.root
-
-        initView()
-
-
-
-
     }
-
-    private fun initView() = with(binding){
-        rv.run {
-
-
-        }
-
-    }
-
-
 
     //앱이 화면에서 삭제 시 메모리에서 삭제가 실행되는 코드!!!!!
-
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
-
+        _binding = null
     }
+
 }
