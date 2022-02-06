52469a05d090ea5c29a41d2863af9f97c31223ce
amy <melow2@naver.com>
2022-02-06 14:55:48 +0900

    di - 네트워크 모듈 작성.

---
+++ app/src/main/java/com/john/episode/di/NetworkModule.kt
@@ -0,0 +1,61 @@
+package com.john.episode.di
+
+import com.john.episode.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    private const val NAME_EPISODE_BASE_URL = "NAME_EPISODE_BASE_URL"
+    private const val EPISODE_BASE_URL = "http://3.34.117.184:9000/"
+
+    @Provides
+    @Named(NAME_EPISODE_BASE_URL)
+    fun provideEpisodeUrl(): String = EPISODE_BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            if (BuildConfig.DEBUG) {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideBaseOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder().apply {
+        interceptors().add(httpLoggingInterceptor)
+    }.run {
+        build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        @Named(NAME_EPISODE_BASE_URL) baseUrl: String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+}
